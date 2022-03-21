79391b36a6c985cebcbecf20ef5f6002c7b8798e
saharla hassan <saharlahassan@outlook.com>
2022-03-21 15:01:37 +0000

    foreignkeytable

---
+++ src/new_table.py
@@ -0,0 +1,42 @@
+import db
+
+#<-------- Create Connection from DB.py -------->
+connection = db.getConnection()
+cursor = connection.cursor()
+
+branch_table = '''CREATE TABLE IF NOT EXISTS branch(
+branch_id SERIAL PRIMARY KEY,
+branch_location VARCHAR(100) NOT NULL
+);
+'''
+
+product_table = '''CREATE TABLE IF NOT EXISTS product(
+product_id SERIAL PRIMARY KEY,
+product_name VARCHAR(100) NOT NULL,
+product_price FLOAT
+);
+'''
+
+transaction_table = '''CREATE TABLE IF NOT EXISTS transaction(
+transaction_id SERIAL PRIMARY KEY,
+date_time TIMESTAMP,
+transaction_total FLOAT NOT NULL,
+branch_id INT,
+CONSTRAINT fk_branch FOREIGN KEY(branch_id) REFERENCES branch(branch_id)
+);
+'''
+
+basket_table = '''CREATE TABLE IF NOT EXISTS basket(
+product_id INT,
+transaction_id INT,
+CONSTRAINT fk_product_id FOREIGN KEY(product_id) REFERENCES product(product_id),
+CONSTRAINT fk_transaction FOREIGN KEY(transaction_id) REFERENCES transaction(transaction_id)
+);
+'''
+
+
+cursor.execute(branch_table)
+cursor.execute(product_table)
+cursor.execute(transaction_table)
+cursor.execute(basket_table)
+connection.commit()
